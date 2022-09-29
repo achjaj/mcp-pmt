884378a8fce0d49b2512641a9ce6e81b62a94eed
Jakub Bucko <bucko.jakub@protonmail.com>
2022-09-29 16:59:56 +0200

    Remove old files

--- simulácie/tlm/tlm.jl
+++ simulácie/tlm/tlm.jl
@@ -1,16 +1,59 @@
+import JSON
 using Trapz
-using CairoMakie
 using DelimitedFiles
-using Polynomials
 
-CairoMakie.activate!(type = "svg")
+struct Parameters
+    Vs::Float64     # V
+    C::Float64      # F
+    R::Float64      # Ω
+    Lmcp::Float64   # m
+    k::Float64      # m⁻¹
+    G::Float64
+    Qs::Float64     # C
+    L::Float64
+    vs::Float64     # V
+    RC::Float64
 
-#= layout of matrices: 
-    ---> t
-    |
-    |
-    v x
-=#
+    Parameters(dict::Dict{Symbol, Any}) = begin
+        args = [Float64(dict[fn]) for fn in fieldnames(Parameters)[1:end-5]]
+
+        G = :G in keys(dict) ? Float64(dict[:G]) : log(args[1]/Float64(dict[:Vm]))
+        Qs = args[1]*args[2]
+        L = args[5]*args[4]
+        vs = args[1]/L
+        RC = args[2]*args[3]
+
+        new(args..., G, Qs, L, vs, RC)
+    end
+end
+
+# function to parse times and xcoords
+function parseDomain(obj::Dict{Symbol, Any})
+    if obj[:type] == "list"
+        return Float64.(obj[:value])
+    elseif  obj[:type] == "range"
+        return eval(Meta.parse("range($(Float64(obj[:min])), $(Float64(obj[:max])), $(:len in keys(obj) ? "length=$(Int(obj[:len]))" : "step=$(Float64(obj[:step]))"))"))
+    end
+
+    throw(DomainError(obj[:type], "Unrecognized type"))
+end
+
+# function to parse i0 and psi0
+function parseInput(obj::Dict{Symbol, Any}, domain)
+    type = obj[:type]
+
+    if type == "constant"
+        return fill(Float64(obj[:value]), length(domain))
+    elseif  type == "list"
+        return Float64.(obj[:value])
+    elseif type == "function"
+        expr = Meta.parse(obj[:expr])
+        f = eval(expr)
+        return Base.invokelatest.(f, domain)
+    end
+
+    throw(DomainError(type, "Unrecognized type"))
+end
 
 # redefine log function to not throw error when argument is negative, but return NaN
 function ntlog(arg)
@@ -22,185 +65,104 @@ function ntlog(arg)
     log(arg)
 end
 
-# MCP-PMT parameters:
+function calculateGain(par::Parameters, i₀, ψ₀, times, xcoords)
+    tsize = length(times)
+    xsize = length(xcoords)
+    tind = 1:tsize
+    xind = 1:xsize
 
-#d = 1e-5        # m; channel diameter
-#A = 6.45e-6     # m²; single anode area
+    ϵs = [range(0.1, 1, length = 10); fill(1, 20)]
+    N = length(ϵs)
+    ϵind = 1:N
 
-#S = π*(d/2)^2   # channel cross section area
-#Nc = A/S        # approx. number of channels
+    gs = Array{Float64}(undef, xsize, tsize, N+1)
+    QQss = Array{Float64}(undef, xsize, tsize, N)
+    QwQss = Array{Float64}(undef, tsize, N)
+    ψs = Array{Float64}(undef, xsize, tsize, N)
 
-Vs = 2400       # V
-Vm = 1100       # V
-C = 2.86e-13    # F
-R = 6.09e8      # Ω
-Lmcp = 1.5e-3   # m
-G = log(Vs/Vm)
-k = 1.16e4      # m⁻¹
-
-Qs = Vs*C
-L = k*Lmcp
-vs = Vs/L 
-
-# space and time discretization
-tsize = 100
-xsize = 100
-sizes = (xsize, tsize)
-tind = 1:tsize
-xind = 1:xsize
-
-xcoords = range(0, L, length = xsize)
-times = range(0, 5e-8, length = tsize)
-
-ψ₀ = fill(0, xsize)
-
-# prepare the for loop
-ϵs = [range(0.1, 1, length = 10); fill(1, 20)]
-N = length(ϵs)
-ϵind = 1:N
-
-# input signal
-iₗ = Polynomial(readdlm("output-signal-poly.csv")[:, 1]).(times)
-#i₀ = hcat([[zeros(100); fill(1e-11, 100)] for i in 1:10]...)[1:tsize]
-
-#= series of gauss shaped peaks
-bell_f(a, b, c) = x -> a*exp(-((x-b)^2)/c)
-bells = [bell_f(1e-11, b, 1e-10) for b in 0:1e-4:1e-3]
-i₀f(t) = sum(map(bf -> bf(t), bells))
-i₀ = i₀f.(times)=#
-
-function calcGain(i₀)
-    # input charge as function of time
-    #Q₀ = [trapz(times[1:t], exp.(times[1:t] ./ (R*C)) .* i₀[1:t]) for t in tind]
-
-    # initial gain
-    g₀ = [exp(G*xcoords[x]) for x in xind, t in tind]
-
-    gs = Array{Float64, 3}(undef, xsize, tsize, N+1)
-    gs[:, :, 1] = g₀
-
-    QQss = Array{Float64, 3}(undef, xsize, tsize, N)
-    QwQss = Array{Float64, 2}(undef, tsize, N)
-    ψs = Array{Float64, 3}(undef, xsize, tsize, N)
+    gs[:, :, 1] = [exp(par.G*x) for x in xcoords, t in tind] # g₀
 
     # calculate the gain
     for i in ϵind
         g = gs[:, :, i]
 
-        QQs = [trapz(times[1:t], exp.(times[1:t] ./(R*C)) .* i₀[1:t] .* g[x, 1:t])/Qs for x in xind, t in tind]
+        QQs = [trapz(times[1:t], exp.(times[1:t] ./par.RC) .* i₀[1:t] .* g[x, 1:t])/par.Qs for x in xind, t in tind]
         QQss[:, :, i] = QQs
 
-        QwQs = [trapz(xcoords, QQs[:, t])/L for t in tind]
+        QwQs = [trapz(xcoords, QQs[:, t])/par.L for t in tind]
         QwQss[:, i] = QwQs
 
-        ψ = [(ψ₀[x] + (QwQs[t] - QQs[x, t])*ϵs[i])*exp(-times[t]/(R*C)) for x in xind, t in tind]
+        ψ = [(ψ₀[x] + (QwQs[t] - QQs[x, t])*ϵs[i])*exp(-times[t]/par.RC) for x in xind, t in tind]
         ψs[:, :, i] = ψ
 
-        new_g = [exp(G*xcoords[x] + trapz(xcoords[1:x], ntlog.(1 .+ ψ[1:x, t]))) for x in xind, t in tind]
+        new_g = [exp(par.G*xcoords[x] + trapz(xcoords[1:x], ntlog.(1 .+ ψ[1:x, t]))) for x in xind, t in tind]
         gs[:, :, i+1] = new_g
     end
 
     gs, QQss, QwQss, ψs
 end
 
-function backwardGain()
-    h₀ = [exp(G*(xcoords[x] - L)) for x in xind, t in tind]
+function dumpTensor(tensor, steps, out::String; array = false)
+    mkpath(out)
 
-    hs = Array{Float64, 3}(undef, xsize, tsize, N+1)
-    hs[:, :, 1] = h₀
-
-    QQss = Array{Float64, 3}(undef, xsize, tsize, N)
-    QwQss = Array{Float64, 2}(undef, tsize, N)
-    ψs = Array{Float64, 3}(undef, xsize, tsize, N)
-
-    for i in ϵind
-        h = hs[:, :, i]
-
-        QQs = [trapz(times[1:t], iₗ[1:t] .* h[x, 1:t])/Qs for x in xind, t in tind]
-        QQss[:, :, i] = QQs
-
-        QwQs = [trapz(xcoords, QQs[:, t])/L for t in tind]
-        QwQss[:, i] = QwQs
-
-        ψ = [ψ₀[x] + (QwQs[t] - QQs[x, t])*ϵs[i] for x in xind, t in tind]
-        ψs[:, :, i] = ψ
-
-        new_h = [exp(G*(xcoords[x]-L) - trapz(xcoords[x:end], ntlog.(1 .+ ψ[x:end, t]))) for x in xind, t in tind]
-        hs[:, :, i+1] = new_h
+    for n in steps
+        path = joinpath(out, "$n.csv")
+        array ? writedlm(path, tensor[:, n]) : writedlm(path, tensor[:, :, n])
     end
-
-    hs, QQss, QwQss, ψs
 end
 
-hs = backwardGain()[1]
-i₀ = iₗ .* hs[1, :, end]
-
-gs, QQss, QwQss, ψs = calcGain(i₀)
-
-# calculate the signal current i(x, t)
-g_final = gs[:, :, end]
-i = [i₀[t] * g_final[x, t] for x in xind, t in tind]
-
-# calculate the excess volateg Φ(x, t)
-ψ_final = ψs[:, :, end]
-Φ = [vs*trapz(xcoords[1:x], ψ_final[1:x, t]) for x in xind, t in tind]
-
-# calculate the channel voltage V(x, t)
-V = [vs*xcoords[x] + Φ[x, t] for x in xind, t in tind]
-
-
-function dumpSteps(cartInd, gs, QQss, QwQss, ψs)
-    dir = "dump"
-    mkpath(dir)
-
-    writedlm("$dir/g", gs[cartInd...])
-    writedlm("$dir/QQs", QQss[cartInd...])
-    writedlm("$dir/QwQs", QwQss[cartInd[2], cartInd[3]])
-    writedlm("$dir/psi", ψs[cartInd...])
+function dumpTensor(tensor, out::String; array = false)
+    dumpTensor(tensor, 1:size(tensor)[end], out; array = array)
 end
 
-function plotGains(n, xinds, tinds, legend=true)
-    plt = Figure(resolution = (600, 460))
-    ax = Axis(
-        plt[1, 1],
-        yscale = log10,
-        xlabel = "z [mm]",
-        ylabel = "g(z, t)",
-        spinewidth = 3,
-        xgridwidth = 3,
-        ygridwidth = 3,
-        xlabelsize = 25,
-        ylabelsize = 25,
-        xticklabelsize = 25,
-        yticklabelsize = 25
-        )
+# MAIN
+conf = JSON.parsefile("sim-param.json", dicttype = Dict{Symbol, Any})
 
-    for t in tinds
-        lines!(ax, xcoords[xinds] ./k .* 1e3, gs[xinds, t, n], label = "t = $(round(times[t] * 1e9, sigdigits=2)) ns", linewidth=3)
+params = Parameters(conf)
+xcoords = parseDomain(conf[:xcoords])
+times = parseDomain(conf[:times])
+
+i₀ = parseInput(conf[:i0], times)
+ψ₀ = parseInput(conf[:psi0], xcoords)
+
+gs, QQss, QwQss, ψs = calculateGain(params, i₀, ψ₀, times, xcoords)
+
+# output
+tesnsors = ["gs", "QQss", "ψs"]
+translate = Dict("i0" => i₀, "psi0" => ψ₀)
+
+outputDir = String(conf[:outputDir])
+mkpath(outputDir)
+
+writedlm(joinpath(outputDir, "times.csv"), times)
+writedlm(joinpath(outputDir, "xcoords.csv"), xcoords)
+write(joinpath(outputDir, "parameters.json"), JSON.json(params, 2))
+
+if conf[:output] == "all"
+    writedlm(joinpath(outputDir, "i0.csv"), i₀)
+    writedlm(joinpath(outputDir, "psi0.csv"), ψ₀)
+
+    for tensor in tesnsors
+        dumpTensor(eval(Meta.parse(tensor)), joinpath(outputDir, tensor))
     end
 
-    legend && axislegend(ax, position=:rb, labelsize=20)
-    plt
-end
+    dumpTensor(QwQss, joinpath(outputDir, "QwQss"); array = true)
 
-function plotPulse(xinds, tinds, z, legend = true)
-    plt = Figure(resolution = (600, 460))
-    ax = Axis(plt[1, 1],
-        xlabel = "t [ns]",
-        ylabel = "i($z, t) [A]",
-        spinewidth = 3,
-        xgridwidth = 3,
-        ygridwidth = 3,
-        xlabelsize = 25,
-        ylabelsize = 25,
-        xticklabelsize = 25,
-        yticklabelsize = 25
-        )
+else
+    for var in String.(conf[:output])
+        parts = split(var, "&")
 
-    for x in xinds
-        lines!(ax, times[tinds] .* 1e9, i[x, tinds], label = "z = $(round(xcoords[x]/k *1e3, sigdigits=2)) mm", linewidth=3)
+        if parts[1] in tesnsors || parts[1] == "QwQss"
+            array = parts[1] == "QwQss"
+
+            if length(parts) > 1
+                dumpTensor(eval(Meta.parse(parts[1])), eval(Meta.parse(parts[2])), joinpath(outputDir, parts[1]); array = array)
+            else
+                dumpTensor(eval(Meta.parse(parts[1])), joinpath(outputDir, parts[1]); array = array)
+            end
+
+        else
+            writedlm(joinpath(outputDir, "$(parts[1]).csv"), translate[parts[1]])
+        end
     end
-
-    legend && axislegend(ax, position=:rb)
-    plt
 end
