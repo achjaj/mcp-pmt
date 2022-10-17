876d7bbade6cad7ef8f4a3d69530a3e1a76409fb
Jakub Bucko <bucko.jakub@protonmail.com>
2022-10-17 17:31:23 +0200

    Working 3d simulation

--- simulácie/monte-carlo/3d/3d.jl
+++ simulácie/monte-carlo/3d/3d.jl
@@ -2,7 +2,7 @@ using Pkg
 
 # check required packages
 println("Checking for dependencies")
-pkgs = ["Distributions", "GLMakie", "GeometryBasics", "LinearAlgebra"]
+pkgs = ["Distributions", "GLMakie", "GeometryBasics"]
 
 # get list of installed packages
 buffer = IOBuffer()
@@ -14,7 +14,7 @@ installed = [line[end-1] for line in split.(readlines(buffer)[2:end-1], " ")]
 mis = filter(pkg -> !(pkg in installed), pkgs)
 isempty(mis) || Pkg.add.(mis)
 
-println("Continuing with the initialization")
+println("Continuing with the initialization")#
 using Distributions
 using GLMakie
 using GeometryBasics
@@ -29,10 +29,13 @@ q = 1.6e-19 # C
 m = 9.1e-31 # kg
 k = 1/2
 
+# e⁻ acceleration
+a = q*E/m
+
 # initialize distributions for generating random values
 ϕDist = Cosine(π, π)        # ϕ ∈ <0, 2π>
 θDist = Cosine(π/4,π/4)     # θ ∈ <0, π/2>
-T₀Dist = Rayleigh(1e-20)
+v₀Dist = Rayleigh(2e5)     # distribution of magnitude of initial velocity
 
 # generate random vector with magnitude mag; default magitude is 1
 function rndVec(mag = 1.0)
@@ -44,38 +47,14 @@ function rndVec(mag = 1.0)
     return mag*[sin(θ)*cos(ϕ), sin(θ)*sin(ϕ), cos(θ)], ϕ, θ
 end
 
-# test the generation of random vectors; creates 3D plot of vector endpoints
-function rndVectorTest(n::Int = 10000)
-    vcs = map(x -> getindex(rndVec(), 1), 1:n)
-
-    vcx = map(v -> getindex(v, 1), vcs)
-    vcy = map(v -> getindex(v, 2), vcs)
-    vcz = map(v -> getindex(v, 3), vcs)
-
-    meshscatter(vcx, vcy, vcz, color = :red, markersize = 1e-2)
-end
-
-# calculates rotatin matrix
-function rotMat(xz)
-    β = -2*asin(norm(xz)/(2*R))
-
-    return [cos(β) 0 sin(β)
-                0  1 0
-            -sin(β) 0 cos(β)]
-end
-
 # calculates the path of an e⁻
-xz(v₀, t) = v₀*t
-y(v₀, t) = v₀*t + q*E*(t^2)/m
-
-# calculates the speed of e⁻ in y direction
-vy(v₀, t) = v₀ + q*E*t/m
+r(v₀, t) = v₀*t + [0, a*t^2, 0]
 
 # calculates the kinetic energy of e⁻
-T(v₀, t) = m*(v₀[1]^2 + vy(v₀[2], t)^2 + v₀[3]^2)/2
+T(v₀, t) = m*(norm(v₀*t + [0, a*t, 0])^2)/2
 
 # calculates the time of collision
-tc(vx, vz) = 2R*vz/(vx^2 + vz^2)
+tc(v) = 2R*v[3]/(v[1]^2 + v[3]^2)
 
 # creates plot with a cylinder, i. e. our channel
 function preparePlot()
@@ -85,54 +64,48 @@ function preparePlot()
 
     # draw the cylinder in red color and transparency
     mesh!(ax, Cylinder{3, Float32}(Point3f0(0.0, 0.0, R), Point3f0(0.0, L, R), R), color = (:red, 0.3), transparency=true)
-
+    
     return ax, plt
 end
 
-# draw one step
-function drawStep(ax, s, rot, r₀; res = 100)
-    times = range(0, s[3], length = res)
-    v₀ = s[2]
 
-    # calculates the path and converts it to global coordinate system
-    vec = hcat([rot*[xz(v₀[1], t), y(v₀[2], t), xz(v₀[3], t)] + r₀ for t in times]...)
+function drawSteps(steps, res = 100)
+    ax, plt = preparePlot()
 
-    # draw the path
-    lines!(ax, vec[1, :], vec[2, :], vec[3, :], color = :blue)
-
-    # highlight start and collision position: green dot is the start position, red is the collision position
-    scatter!(ax, vec[1, [1, end]], vec[2, [1, end]], vec[3, [1, end]], color = [:green, :red])
-end
-
-# draw all the steps
-function drawSteps(ax, steps)
-    globr₀ = zeros(3)
-
-    for (i, s) in enumerate(steps)
-        println("Drawing step $i; r₀: $globr₀")
-        rot = rotMat(globr₀[[1, 3]])
-        drawStep(ax, s, rot, globr₀)
-
-        v₀ = s[2]
-        t = s[3]
-        S = s[4]
+    l = 0
+    d_glob = zeros(3)
+    for s in steps
+        d = r(s[1:2]...)
         
-        globr₀ = rot*([xz(v₀[1], t), S, xz(v₀[3], t)] * globr₀)
+        l += 2*R*asin(norm(d[[1, 3]])/R/2)
+        if l > 2π*R
+            l -= R
+        end
+
+        β = l/R
+
+        d_glob_new = [R*cos(β), d_glob[2] + d[2], R + R*sin(β)]
+        v = ([d_glob_new[1], d_glob_new[2] - a*s[2]^2, d_glob_new[3]] - d_glob) / s[2]
+
+        times = range(0, s[2], length = res)
+        lines!(ax, hcat([r(v, t) + d_glob for t in times]...), color = :blue)
+
+        d_glob = d_glob_new
     end
+
+    plt, ax
 end
 
 # calculates one step
 function step()
-    T₀ = rand(T₀Dist)           # initial energy
-    magv₀ = sqrt(2*T₀/m)
+    magv₀ = rand(v₀Dist)
     v₀, ϕ, θ = rndVec(magv₀)
-    t = tc(v₀[1], v₀[3])        # time of collision
+    t = tc(v₀)                 # time of collision
 
-    S = y(v₀[2], t)
     Tc = T(v₀, t)              # collision energy
     δ = k*Tc
 
-   return T₀, v₀, t, S, Tc, δ, ϕ, θ
+   return v₀, t, δ, r(v₀, t)[2], ϕ, θ
 end
 
 # run the simulation and draw plot of the paths
@@ -146,8 +119,7 @@ function run()
         println("Step $n:\n\tS = $(sum([s[4] for s in steps]))")
     end
 
-    ax, plt = preparePlot()
-    drawSteps(ax, steps)
+    plt, ax = drawSteps(steps)
     display(plt)
 
     return steps, plt
